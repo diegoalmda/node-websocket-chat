d94cebb58e18df8d5eef7f352a9ab3cb44f8af44
Diego Almeida <diegoalmda@gmail.com>
2022-07-13 13:54:08 -0300

    Chat connection and chat styles

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
 server.listen(3333);
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+  console.log("Conexão detectada...");
+
+  
+});
