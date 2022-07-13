af8e1298c099bbdf1087b528904ced468e4301f2
Diego Almeida <diegoalmda@gmail.com>
2022-07-13 15:33:58 -0300

    Sending and receiving messages

--- server.js
+++ server.js
@@ -11,8 +11,41 @@ server.listen(3333);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let connectedUsers = [];
+
 io.on('connection', (socket) => {
   console.log("Conexão detectada...");
 
-  
+  socket.on('join-request', (username) => {
+    console.log("Nome: ", username)
+    socket.username = username;
+    connectedUsers.push(username);
+    console.log(connectedUsers);
+
+    socket.emit('user-ok', connectedUsers);
+    socket.broadcast.emit('list-update', {
+      joined: username,
+      list: connectedUsers
+    });
+  });
+
+  socket.on('disconnect', () => {
+    connectedUsers = connectedUsers.filter(user => user !== socket.username);
+    console.log(connectedUsers);
+
+    socket.broadcast.emit('list-update', {
+      left: socket.username,
+      list: connectedUsers
+    });
+  });
+
+  socket.on('send-msg', (txt) => {
+    let obj = {
+      username: socket.username,
+      message: txt
+    };
+
+    // socket.emit('show-msg', obj);
+    socket.broadcast.emit('show-msg', obj);
+  });
 });
